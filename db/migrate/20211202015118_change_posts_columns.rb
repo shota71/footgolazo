a5a04c03f259ec5a5d1fef20e88ea74e6cbd12bc
shota <shota371007@gmail.com>
2021-12-19 00:55:10 +0900

    Update application

--- db/migrate/20211202015118_change_posts_columns.rb
+++ db/migrate/20211202015118_change_posts_columns.rb
@@ -1,5 +1,5 @@
 class ChangePostsColumns < ActiveRecord::Migration[6.1]
   def change
-    change_column :posts, :user_id, :integer
+    change_column :posts, :user_id, "integer USING CAST(user_id AS integer)"
   end
 end
